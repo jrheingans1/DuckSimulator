d70af8e9877089e0cb0d7a04829c4c95e49f5469
James <James@James-PC>
2014-02-24 19:22:20 -0600

    fixed FlyRocketPowered class

--- src/version5/FlyRocketPowered.java
+++ src/version5/FlyRocketPowered.java
@@ -1,9 +1,9 @@
 package version5;
 
-import version5.FlyGroup;
+public class FlyRocketPowered implements FlyStrategy {
 
-public class FlyRocketPowered implements FlyGroup {
-	public void fly() {
-		System.out.println("I'm flying with a rocket");
-	}
+    @Override
+    public void fly() {
+        System.out.println("I'm flying with a rocket");
+    }
 }
